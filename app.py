3a65b7a94595117d23ba0aca3145e4e5bcd9bf51
Niranjan <ngnalu4@gmail.com>
2025-12-07 11:03:53 +0530

    Added Render port support

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
@@ -15,4 +16,6 @@ def handle_message(msg):
     send(msg, broadcast=True)  # send message to everyone
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0',port=5000)
+    port = int(os.environ.get("PORT",5000))
+    socketio.run(app,host='0.0.0.0',port=port)
+      
